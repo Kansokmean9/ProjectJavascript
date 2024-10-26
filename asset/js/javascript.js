a22ec28e11ac345db9b91d05fd6e6fdf409117a5
Kansokmean9 <kansokmean006@gmail.com>
2024-10-26 16:29:55 +0700

    commits

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -11,3 +11,22 @@
             alert('Notifications feature coming soon!');
             location.reload();
         });
+
+        // get me
+        let id = sessionStorage.getItem('Token');
+        console.log(id);
+
+        fetch(`https://mps7.chandalen.dev/api/me`, {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${id}`
+            }
+        })
+            .then(res => res.json())
+            .then(json => {
+                // console.log(json);
+                document.getElementById('avtadmin').src = json.data.avatar;
+                document.getElementById('adminName').innerHTML = json.data.name;
+
+            })
