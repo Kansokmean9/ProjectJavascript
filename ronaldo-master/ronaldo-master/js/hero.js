ddab5148a32c109f6aa00e2a85c69a35a1122f77
Your Name <you@example.com>
2024-11-04 16:07:29 +0700

    add

--- ronaldo-master/ronaldo-master/js/hero.js
+++ ronaldo-master/ronaldo-master/js/hero.js
@@ -13,34 +13,40 @@ function fetchAbout() {
         const profile = json.data.profile;
         const Bio = json.data.bio;
         const social = json.data.social;
-        const About = `
 
-          <div class="col-lg-6 col-md-6  align-items-center">
+        // Name header
+        const name = document.getElementById('navbar-name');
+        let names = document.getElementById('names');
+        name.innerHTML = profile.en_first_name[0];
+        names.innerHTML = profile.en_first_name + " " + profile.en_last_name;
+
+        // Image footer
+        const image_contact = document.getElementById('image-contact');
+        image_contact.src = profile.photo_about;
+
+        const About = `
+          <div class="col-lg-6 col-md-6 align-items-center">
             <div class="text text-center">
               <span class="subheading">Hey! I am</span>
               <h2>${profile.en_first_name} ${profile.en_last_name}</h2>
               <h3>${Bio.en_hero_bio}</h3>
               <div class="social-icons">
-              <a href="${social.facebook}" target="_blank">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://t.me/tchandalen" target="_blank">
-              <i class="fab fa-telegram-plane"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/chandalen-teang" target="_blank">
-              <i class="fab fa-linkedin-in"></i>
-            </a></div>
-           
+                <a href="${social.facebook}" target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="https://t.me/tchandalen" target="_blank">
+                  <i class="fab fa-telegram-plane"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/chandalen-teang" target="_blank">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
             </div>
           </div>
-          <div class="col-12 col-md-6 image_hero ">
-          <div class="img w-100 h-100" style="background-image: url(${profile.photo_hero})"></div>
-
-
+          <div class="col-12 col-md-6 image_hero">
+            <div class="img w-100 h-100" style="background-image: url(${profile.photo_hero}); width: 600px; border-radius: 10px; background-size: cover; background-position: center;"></div>
           </div>
-         
-           
-          `;
+        `;
 
         document.getElementById("hero-section").innerHTML = About;
         console.log(json.data);
